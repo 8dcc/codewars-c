b805b05c203679d57a0225e14d3a833f77997054
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-25 00:27:16 +0200

    Add calculate-bmi.c

---
+++ calculate-bmi.c
@@ -0,0 +1,9 @@
+const char *bmi (int weight, double height)
+{
+    double result = weight / (height * height);
+    if (result <= 18.5) return "Underweight";
+    if (result <= 25.0) return "Normal";
+    if (result <= 30.0) return "Overweight";
+    if (result > 30)    return "Obese";
+    return "Obese";
+}
