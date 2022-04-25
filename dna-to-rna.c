f0208b674e7358c8c7a0789c5ce7c07795654bf9
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-25 15:44:49 +0200

    Replace array size check with sizeof -> strlen() + 1

--- dna-to-rna.c
+++ dna-to-rna.c
@@ -5,7 +5,7 @@
 
 char *dna_to_rna(const char *dna) {   
     // Did not work because I was pointing to dna, and trying to change a const
-    char *rna = malloc(sizeof(dna)/sizeof(char));
+    char *rna = malloc(strlen(dna) + 1);
     strcpy(rna, dna);
     for (int n = 0; *(dna+n) != '\0'; n++) {
         if (dna[n] == 'T') rna[n] = 'U';
