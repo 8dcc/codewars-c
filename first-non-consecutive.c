733f1ba5fc15986fad83ca044f39c83055352700
8dcc <8dcc.git@gmail.com>
2023-04-01 16:03:31 +0200

    Fix first-non-consecutive.c

--- first-non-consecutive.c
+++ first-non-consecutive.c
@@ -1,15 +1,32 @@
 /* Kata: https://www.codewars.com/kata/58f8a3a27a5c28d92e000144 */
-/* Not complete */
 
 #include <stdbool.h>
 
 bool firstNonConsecutive (const int arr[], const int length, int *first) {
-    const int step = -arr[0] + arr[1];  // TODO
-    for (int n = 0; n < length; n++) {
-        if (arr[n] != arr[n-1] + step) {
+    for (int i = 1; i < length; i++) {
+        if (arr[i] != arr[i-1] + 1) {
+            *first = arr[i];
+            return true;
+        }
+    }
+    return false;
+}
+
+#ifdef DIFFERENT_STEPS
+#include <stdlib.h>
+
+#define STEP(a, b) \
+    { abs(b) - abs(a); }
+
+bool firstNonConsecutive(const int arr[], const int length, int *first) {
+    int first_step = STEP(arr[0], arr[1]);
+    for (int i = 0; i < length; i++) {
+        if (/*TODO*/) {
             *first = arr[n];
             return true;
         }
     }
     return false;
 }
+#endif /* DIFFERENT_STEPS */
+
